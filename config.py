a9f8d9ec273a3f65a0c2125a7a6a6bcebaaaa432
NickWu97 <n0970711111@gmail.com>
2026-05-16 00:10:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,27 @@
 
+# config.py
+import json
+import os
+
+class Config:
+    def __init__(self, config_path="config.json"):
+        self.config_path = config_path
+        self.data = self._load()
+    
+    def _load(self):
+        if os.path.exists(self.config_path):
+            with open(self.config_path, 'r', encoding='utf-8') as f:
+                return json.load(f)
+        return {}
+    
+    def get(self, key, default=None):
+        keys = key.split('.')
+        d = self.data
+        for k in keys:
+            if isinstance(d, dict):
+                d = d.get(k)
+            else:
+                return default
+        return d if d is not None else default
+
+config = Config()
